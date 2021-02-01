5a852a291d3d384eed2ec961a2c851c9a8847177
Karyn <karyn.herrera@gmail.com>
2021-02-01 18:07:28 -0300

    se añade vista de admin y componentes para buscador y tabla

---
+++ src/components/inputSearch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="campo">
+    <input v-model="codigo" class="input" />
+    <button type="button">
+      <svg
+        width="38"
+        height="38"
+        viewBox="0 0 38 38"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M37.4805 32.8566L30.0809 25.457C29.7469 25.123 29.2941 24.9375 28.8191 24.9375H27.6094C29.6578 22.3176 30.875 19.0223 30.875 15.4375C30.875 6.90977 23.9652 0 15.4375 0C6.90977 0 0 6.90977 0 15.4375C0 23.9652 6.90977 30.875 15.4375 30.875C19.0223 30.875 22.3176 29.6578 24.9375 27.6094V28.8191C24.9375 29.2941 25.123 29.7469 25.457 30.0809L32.8566 37.4805C33.5543 38.1781 34.6824 38.1781 35.3727 37.4805L37.473 35.3801C38.1707 34.6824 38.1707 33.5543 37.4805 32.8566ZM15.4375 24.9375C10.1902 24.9375 5.9375 20.6922 5.9375 15.4375C5.9375 10.1902 10.1828 5.9375 15.4375 5.9375C20.6848 5.9375 24.9375 10.1828 24.9375 15.4375C24.9375 20.6848 20.6922 24.9375 15.4375 24.9375Z"
+          fill="#CCCCCC"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@design';
+
+@media (min-width: 600px) {
+  .campo {
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: inset 4px 4px 4px rgba(0, 0, 0, 0.25);
+    height: 42px;
+    width: fit-content;
+    position: relative;
+    input {
+      border: none;
+      border-radius: 4px;
+      width: 100%;
+      height: 100%;
+      background-color: transparent;
+      padding: 0 15px;
+      &:focus {
+        outline: none;
+      }
+    }
+    button {
+      position: absolute;
+      right: -3px;
+      width: 12%;
+      top: 3px;
+      background: transparent;
+      border: none;
+      cursor: pointer;
+      svg {
+        width: 50%;
+      }
+    }
+  }
+}
+</style>
